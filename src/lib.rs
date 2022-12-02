465d936487ecfdd071cedbc3a69d91a35cc1873f
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-12-02 12:21:19 +0700

    calorie counting.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,25 @@
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn calorie_counting() {
+        let contents = fs::read_to_string("./data/calorie-count")
+            .expect("Something went wrong reading the file");
+
+        let mut largest_calorie = 0;
+        for curr in contents.split("\n\n") {
+            let mut total_calorie = 0;
+            for c in curr.split("\n") {
+                let calorie_int = c.parse::<i32>().unwrap();
+                total_calorie += calorie_int;
+            }
+
+            if largest_calorie < total_calorie {
+                largest_calorie = total_calorie;
+            }
+        }
+
+        println!("Largest calorie: {}", largest_calorie);
     }
 }
