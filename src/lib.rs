df94629fe2619804b22387174840b48894d05f4c
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-12-02 12:29:33 +0700

    top three total calories.

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ mod tests {
         let contents = fs::read_to_string("./data/calorie-count")
             .expect("Something went wrong reading the file");
 
-        let mut largest_calorie = 0;
+        let mut top_three = vec![0, 0, 0];
         for curr in contents.split("\n\n") {
             let mut total_calorie = 0;
             for c in curr.split("\n") {
@@ -15,11 +15,15 @@ mod tests {
                 total_calorie += calorie_int;
             }
 
-            if largest_calorie < total_calorie {
-                largest_calorie = total_calorie;
+            if top_three[0] < total_calorie {
+                top_three.drain(0..1);
+                top_three.push(total_calorie);
+                top_three.sort()
             }
         }
 
-        println!("Largest calorie: {}", largest_calorie);
+        println!(
+            "Top three calorie total: {}", top_three[0] + top_three[1] + top_three[2]
+        );
     }
 }
